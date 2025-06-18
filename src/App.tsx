b2a7b7ec8637a1a5f0c52d006cf15e612e13a4f4
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2025-06-18 13:02:30 +0300

    Add Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Analytics } from '@vercel/analytics/react';
+
 import { Main } from './pages/Main';
 
 export const App = () => {
-  return <Main />;
+  return (
+    <>
+      <Main />
+      <Analytics />
+    </>
+  );
 };
