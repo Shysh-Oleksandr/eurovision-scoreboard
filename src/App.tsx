8363e0ede12b6c4d9294864a916ccd3ad591af1a
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2025-06-23 21:53:18 +0300

    Remove Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import { Analytics } from '@vercel/analytics/react';
-
 import { Main } from './pages/Main';
 
 export const App = () => {
   return (
     <>
       <Main />
-      {!window.location.hostname.includes('localhost') && <Analytics />}
     </>
   );
 };
