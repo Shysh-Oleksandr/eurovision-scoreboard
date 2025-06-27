c02096bbcb374403f32aee974485cb0518a99bb9
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2025-06-27 17:13:42 +0300

    Add the 2021 theme

--- src/state/countriesStore.ts
+++ src/state/countriesStore.ts
@@ -9,7 +9,7 @@ import { getThemeForYear } from '../theme/themes';
 import { themesInfo } from '../theme/themesInfo';
 import { Theme, ThemeInfo } from '../theme/types';
 
-const INITIAL_YEAR = '2025';
+const INITIAL_YEAR = '2021';
 
 interface CountriesState {
   // State
