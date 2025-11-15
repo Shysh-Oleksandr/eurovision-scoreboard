fb9af8c9d88589bc5c90cf534021cc8ebdbce5c6
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2025-11-15 12:14:51 +0200

    fix: set expireTime to 0 to prevent issue with chunk loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
+  expireTime: 0,
 };
 
 export default nextConfig;
